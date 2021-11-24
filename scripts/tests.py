93157de374332bbf849f16aad45aeb7fbee77789
Daniel Evers <dermojo@gmail.com>
2021-11-24 22:31:21 +0100

    added PUB/SUB test and TODOs

--- scripts/tests.py
+++ scripts/tests.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import subprocess
+import sys
 import time
 import unittest
+import uuid
 
 import pika
 
@@ -13,20 +15,25 @@ DOCKER_CLI = ('docker', 'run', '-it', '--rm', '--name', CONTAINER_NAME,
 
 class RabbitMQTests(unittest.TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         # start a rabbitmq container
-        self._log = open('server.log', 'wt')
-        self._proc = subprocess.Popen(DOCKER_CLI, stdout=self._log, stderr=subprocess.STDOUT)
+        sys.stderr.write('Starting container\n')
+        cls._log = open('server.log', 'wt')
+        cls._proc = subprocess.Popen(
+            DOCKER_CLI, stdout=cls._log, stderr=subprocess.STDOUT)
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         # stop the container again
-        self._proc.terminate()
-        self._proc.wait()
-        self._proc = None
-        self._log.close()
-        self._log = None
-    
-    def _receiveOnce(self, chan : pika.BlockingConnection, queue, auto_ack=True):
+        sys.stderr.write('Stopping container\n')
+        cls._proc.terminate()
+        cls._proc.wait()
+        cls._proc = None
+        cls._log.close()
+        cls._log = None
+
+    def _receiveOnce(self, chan: pika.BlockingConnection, queue, auto_ack=True):
         for method, properties, body in chan.consume(queue=queue, auto_ack=auto_ack):
             chan.cancel()
             return method, properties, body
@@ -43,9 +50,13 @@ class RabbitMQTests(unittest.TestCase):
                 time.sleep(sleep)
         raise RuntimeError('connect failed')
 
+    def _getQueueName(self):
+        '''Generates a new random queue name'''
+        return str(uuid.uuid4())
+
     def testHelloWorld(self):
         '''Send & receive a message'''
-        queue = 'hello'
+        queue = self._getQueueName()
         message = b'Hello World!'
 
         srvConn, srvChannel = self._conn()
@@ -55,11 +66,45 @@ class RabbitMQTests(unittest.TestCase):
         clientConn, clientChannel = self._conn()
         clientChannel.queue_declare(queue=queue)
         method, properties, body = self._receiveOnce(clientChannel, queue=queue)
-        
+
         self.assertEqual(body, message)
         self.assertNotEqual(method, None)
         self.assertNotEqual(properties, None)
 
+    def testPubSub(self):
+        '''Publish a message to a subscriber'''
+        exchange = 'pubsub_test_exchange'
+        message = b'This is a publication message.'
+
+        srvConn, srvChannel = self._conn()
+        srvChannel.exchange_declare(exchange=exchange, exchange_type='fanout')
+
+        # client
+        clientConn, clientChannel = self._conn()
+        clientChannel.exchange_declare(exchange=exchange, exchange_type='fanout')
+        # temporary queue, automatically deleted
+        result = clientChannel.queue_declare(queue='', exclusive=True)
+        queue = result.method.queue
+        # subscribe by binding the queue to the exchange
+        clientChannel.queue_bind(exchange=exchange, queue=queue)
+
+        # now publish
+        srvChannel.basic_publish(exchange=exchange, routing_key='', body=message)
+
+        method, properties, body = self._receiveOnce(clientChannel, queue=queue)
+        self.assertEqual(method.exchange, exchange)
+        self.assertEqual(body, message)
+
+# TODO: (see https://www.rabbitmq.com/queues.html)
+# - PUB/SUB & filter
+# - request/response (correlation IDs)
+# - max. message/queue lifetime -> TTL
+#   - queue length limit / policy: https://www.rabbitmq.com/maxlength.html
+# - durability
+#   - note: also affects publishing! (https://www.rabbitmq.com/publishers.html#message-properties)
+# - cluster
+# - custom headers (tags)
+# - C/C++ API
 
 if __name__ == '__main__':
     unittest.main()
